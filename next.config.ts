ae7e111da5ce94afb8ab8b708ff92cf1836ad4a5
R4V3NSH4D0W <lenishmagar@gmail.com>
2025-06-26 18:08:33 +0545

    fix: clean up Next.js config for optimal build performance

--- next.config.ts
+++ next.config.ts
@@ -2,36 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { isServer: _isServer }) => {
-    // Exclude changelog files from builds for better performance
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: [
-        ...(config.watchOptions?.ignored || []),
-        '**/CHANGELOG*.md',
-        '**/changelogs/**',
-        '**/scripts/**',
-        '**/daily/**',
-      ],
-    };
-
-    // Exclude changelog files from being processed by webpack
-    config.module.rules.push({
-      test: /\.(md)$/,
-      include: [/changelogs/, /CHANGELOG/, /daily/],
-      type: 'asset/resource',
-      generator: {
-        emit: false, // Don't emit these files to the build output
-      },
-    });
-
-    return config;
-  },
-
-  // Exclude changelog directories from static file serving
-  async rewrites() {
-    return [];
-  },
 };
 
 export default nextConfig;
